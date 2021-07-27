81c51883f8f7fa09167901df1652d8dfc7afa9ca
Shadowbeetle <tamas.kadlecsik@gmail.com>
2021-07-27 21:05:39 +0200

    add sh flash commands

--- flash/d1mini.sh
+++ flash/d1mini.sh
@@ -1,1 +1,1 @@
-d1mini.bat
+python -m esptool --port COM5 write_flash --flash_freq 80m --flash_size 32m --flash_mode qio 0x0 .\espruino_2v09_esp8266_4mb_combined_4096.bin
--- flash/nodemcu.sh
+++ flash/nodemcu.sh
@@ -1,1 +1,1 @@
-nodemcu.bat
+python -m esptool --port COM5 --baud 115200 write_flash --verify --flash_freq 80m --flash_mode dio --flash_size 32m 0x0000 "boot_v1.6.bin" 0x1000 espruino_esp8266_user1.bin 0x3FC000 esp_init_data_default.bin 0x37E000 blank.bin
