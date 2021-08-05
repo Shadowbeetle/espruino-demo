1fff50f4bdcdb5b0e2cd271cb43bee7509e101eb
Shadowbeetle <tamas.kadlecsik@gmail.com>
2021-08-05 12:14:21 +0200

    get config from heroku

--- client/client.js
+++ client/client.js
@@ -4,7 +4,11 @@ const http = require('http');
 const SSID = 'RisingStack';
 const PASSWORD = 'classlessFreedom!';
 const PORT = 8000;
-const HOST = '192.168.0.60';
+let host;
+
+const CONFIG_SERVER_HOST = 'espruino-demo-config-server.herokuapp.com';
+const CONFIG_SERVER_PROTOCOL = 'https';
+const CONFIG_SERVER_PORT = 0;
 
 const BUTTON = NodeMCU.D1;
 
@@ -12,6 +16,7 @@ BUTTON.mode('input_pullup');
 
 Wifi.connect(SSID, { password: PASSWORD }, (err) => {
   if (!err) {
+    getConfig();
     setWatch(handlePush, BUTTON, {
       repeat: true,
       edge: 'falling',
@@ -22,7 +27,10 @@ Wifi.connect(SSID, { password: PASSWORD }, (err) => {
 });
 
 function handlePush(e) {
-  const req = http.get(`http://${HOST}:${PORT}`, (res) => {
+  if (!host) {
+    getConfig();
+  }
+  const req = http.get(`http://${host}:${PORT}`, (res) => {
     let data = '';
 
     res.on('data', (chunk) => {
@@ -41,4 +49,32 @@ function handlePush(e) {
   req.on('error', (err) => {
     console.log('HTTP ERROR>', err);
   });
+}
+
+function getConfig() {
+  console.log('getting config');
+
+  const req = http.get(
+    `${CONFIG_SERVER_PROTOCOL}://${CONFIG_SERVER_HOST}${
+      CONFIG_SERVER_PORT ? ':' + CONFIG_SERVER_PORT : ''
+    }/`,
+    (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('close', (hadError) => {
+        console.log('CONFIG SERVER REQEST ENDED WITHOUT ERROR>', !hadError);
+        console.log('CONFIG SERVER RESPONSE>', data);
+        console.log(res.statusCode, res.statusMessage);
+      });
+
+      res.on('error', (err) =>
+        console.log('CONFIG SERVER REQUEST ERROR>', err)
+      );
+    }
+  );
+
+  req.on('error', (err) => console.log('CONFIG SERVER REQUEST ERROR>', err));
 }
